85aaf8648ef6a2ba746e40f94f3fc6e9644f328e
polluxtroy3758 <2147396+polluxtroy3758@users.noreply.github.com>
2022-02-25 16:28:27 +0100

    test: improve test coverage

--- tests/test_pywgkey.py
+++ tests/test_pywgkey.py
@@ -1,11 +1,9 @@
+import pathlib
+
 import pytest
 
-from pywgkey import __version__
-from pywgkey.utils import generate_keys_until_string_is_found
-
-
-def test_version():
-    assert __version__ == "0.1.1"
+from pywgkey.key import WgKey, WgPsk
+from pywgkey.utils import generate_keys_until_string_is_found, write_key_to_file
 
 
 def test_WgKey():
@@ -13,6 +11,7 @@ def test_WgKey():
     key = generate_keys_until_string_is_found(wanted_string)
     assert wanted_string in key.pubkey.lower()
     assert key.name == wanted_string
+    assert str(key) == key.pubkey
 
 
 def test_WgKey_begining():
@@ -20,6 +19,7 @@ def test_WgKey_begining():
     key = generate_keys_until_string_is_found(wanted_string, startswith=True)
     assert key.pubkey.lower().startswith(wanted_string)
     assert key.name == wanted_string
+    assert len(key.privkey) == 44
 
 
 def test_invalid_alphabet():
@@ -38,3 +38,36 @@ def test_empty_string():
     wanted_string = ""
     with pytest.raises(ValueError):
         generate_keys_until_string_is_found(wanted_string)
+
+
+def test_WgPsk():
+    key = WgPsk("test")
+    assert key.name == "test"
+    assert len(key.key) == 44
+    assert str(key) == key.key
+
+
+def test_write_key_to_file():
+    key = WgKey()
+    key.name = "testkey"
+    psk = WgPsk(key.name)
+    privkey_path = pathlib.Path(f"{key.name}.priv")
+    pubkey_path = pathlib.Path(f"{key.name}.pub")
+    psk_path = pathlib.Path(f"{key.name}.psk")
+    write_key_to_file(key, psk)
+
+    # Private key file
+    assert privkey_path.exists()
+    assert privkey_path.read_text() == key.privkey
+
+    # Public key file
+    assert pubkey_path.exists()
+    assert pubkey_path.read_text() == key.pubkey
+
+    # Preshared key file
+    assert psk_path.exists()
+    assert psk_path.read_text() == psk.key
+
+    privkey_path.unlink()
+    pubkey_path.unlink()
+    psk_path.unlink()
