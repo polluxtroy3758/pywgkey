4bfd9851ab533d5610bb6b56b54dd5aef31568b3
polluxtroy3758 <2147396+polluxtroy3758@users.noreply.github.com>
2022-03-03 10:15:44 +0100

    refactor!: move from `argparse` to `click` for cli management

--- src/pywgkey/__main__.py
+++ src/pywgkey/__main__.py
@@ -1,55 +1,33 @@
 """This file is used when the module is called directly
 """
 
-import argparse
-import sys
+import click
 
 from .key import WgPsk
-from .utils import (
-    generate_keys_until_string_is_found,
-    print_keys,
-    validate_string,
-    write_key_to_file,
+from .utils import generate_keys_until_string_is_found, print_keys, write_key_to_file
+
+
+@click.command()
+@click.argument("wanted_string", type=str, required=True)
+@click.option(
+    "-b", "--begining", is_flag=True, help="If the pubkey must start with the string"
 )
+@click.option("-w", "--write", is_flag=True, help="Write keys to files")
+@click.option("-p", "--psk", is_flag=True, help="Genarate a preshared key as well")
+def main(wanted_string: str, begining: bool, write: bool, psk: bool):
+    """Generate WireGuard keypair containing specified wanted string"""
 
+    key = generate_keys_until_string_is_found(wanted_string, begining)
 
-def main(args: argparse.Namespace):
-    """Main function used when calling module directly"""
+    pre_shared_key = None
+    if psk:
+        pre_shared_key = WgPsk(wanted_string)
 
-    validate_string(args.string)
-
-    key = generate_keys_until_string_is_found(args.string, args.begining)
-
-    psk = None
-    if args.psk:
-        psk = WgPsk(args.string)
-
-    if args.write:
-        write_key_to_file(key, psk)
+    if write:
+        write_key_to_file(key, pre_shared_key)
     else:
-        print_keys(key, psk)
+        print_keys(key, pre_shared_key)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Generate wg keypair containing specified string",
-        prog="python -m pywgkey",
-    )
-    parser.add_argument(
-        "string", type=str, help="The string that must be found in the pubkey"
-    )
-    parser.add_argument(
-        "-b",
-        "--begining",
-        action="store_true",
-        help="If the pubkey must start with the string (default: False)",
-    )
-    parser.add_argument(
-        "-w", "--write", action="store_true", help="Write keys to files"
-    )
-    parser.add_argument(
-        "-p", "--psk", action="store_true", help="Genarate a preshared key as well"
-    )
-    args = parser.parse_args()
-
-    sys.exit(main(args))
+    main()
